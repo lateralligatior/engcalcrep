06289f69022c291f1cc0be78c7f7ed5a197d5e45
lateralligatior <nate.hilbrands@gmail.com>
2021-05-30 20:36:56 -0400

    vertical

--- vertical/views.py
+++ vertical/views.py
@@ -36,4 +36,7 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'vertical/post_edit.html', {'form': form})
+
+def verticalmoment_load(request):
+    return render(request, 'vertical/vertical.html')
